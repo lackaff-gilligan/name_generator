53ff105b82a0b8481e9ce8ad842430e44148cb7c
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-28 13:36:51 -0500

    added bootstrap, form inputs

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var nameRouter = require('./routes/name_router.js');
+app.use('/name', nameRouter);
+
+// share static files, found in public folder
+app.use(express.static('server/public'));
+
+//start up the server
+app.listen(port, function(){
+    console.log('Server running on port:', port);
+})
