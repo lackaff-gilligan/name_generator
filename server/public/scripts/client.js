4ebe805649c4e861e0c324560e08b3219911aa29
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-29 11:06:30 -0500

    created GET request in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,22 @@
 console.log('hello from client.js');
+$(document).ready(onReady);
+
+function onReady(){
+    refreshBabyNames();
+}
+
+function refreshBabyNames() {
+    // make GET request for all names on db
+    $.ajax({
+        method: 'GET',
+        url: '/names'
+    }).done(function(response){
+        console.log('response from GET req:', response);
+        // Response is the named babies array
+        var namedBabies = response;
+        appendNamedBabiesToDom(namedBabies);
+    }).fail(function(error){
+        // TODO -add alert to user
+        console.log('Something went wrong with GET request');
+    });
+}
